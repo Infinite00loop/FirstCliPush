9779e5b17128feb721a518d00eb5a02786c861a8
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-01 21:48:42 +0530

    dom-manip

--- domManip.js
+++ domManip.js
@@ -8,18 +8,42 @@ var title=document.getElementById('header-title');
 // console.log(title);
 // title.innerText='Testing';
 // console.log(title)
-title.innerHTML='<h2>MY HEADING</h2>'
-var mainTitle=document.getElementById('main-header');
-mainTitle.style.borderBottom='solid 3px #000'
+// title.innerHTML='<h2>MY HEADING</h2>'
+// var mainTitle=document.getElementById('main-header');
+// mainTitle.style.borderBottom='solid 3px #000'
 
-var addItem=document.getElementById('add');
-addItem.style.fontWeight='bold';
-addItem.style.color='green';
+// var addItem=document.getElementById('add');
+// addItem.style.fontWeight='bold';
+// addItem.style.color='green';
 
-var list=document.getElementsByClassName('list-group-item');
-console.log(list);
-list[2].style.backgroundColor='green';
+// var list=document.getElementsByClassName('list-group-item');
+// console.log(list);
+// list[2].style.backgroundColor='green';
 
-for(var i=0;i<list.length;i++){
-    list[i].style.fontWeight='bold';
+// for(var i=0;i<list.length;i++){
+//     list[i].style.fontWeight='bold';
+// }
+
+//using class name
+// var list=document.getElementsByClassName('list-group');
+// console.log(list.items.children);
+// list.items.children[4].style.fontWeight='bold';
+
+// //using tag name
+
+// var list2=document.getElementsByTagName('li');
+// list2[4].style.fontWeight='bold';
+
+// var item2=document.querySelector('.list-group-item:nth-child(2)');
+// item2.style.color='green'
+// var item3=document.querySelector('.list-group-item:nth-child(3)');
+// item3.style.display='none';
+
+// var item=document.querySelectorAll('.list-group-item')
+// item[1].style.color='green';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
